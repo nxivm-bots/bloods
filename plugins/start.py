dd8ea7c80f13772572c44859f79f91a54a65bdd8
nxivm-bots <massacres@myyahoo.com>
2024-11-24 03:02:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -259,12 +259,8 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(text="Join Channel", url=client.invitelink),
-            InlineKeyboardButton(text="Join Channel", url=client.invitelink2),
+            InlineKeyboardButton(text="Join Channel", url=client.invitelink2)
         ],
-        [
-            InlineKeyboardButton(text="Join Channel", url=client.invitelink3),
-            #InlineKeyboardButton(text="Join Channel", url=client.invitelink4),
-        ]
     ]
     try:
         buttons.append(
